dd5616cbd4482702543977490328db0231aa417f
Tejas Gupta <tejas.gupta291@gmail.com>
2024-12-03 15:53:44 -0600

    Adding placeholder for motion capture

---
+++ src/pages/Placeholder/Placeholder.jsx
@@ -0,0 +1,16 @@
+//created a separate route for running this file "/Placeholder"
+//All the imports necessary for displaying animation will be written here
+import { Link } from "react-router-dom";
+
+//Helper functions
+
+//Tween function
+const Tween = () => {
+    return (
+    <div> <p>Placeholder Page</p>
+        <Link to="/"> Back to Home</Link>
+    </div>
+    )
+};
+
+export default Tween;
